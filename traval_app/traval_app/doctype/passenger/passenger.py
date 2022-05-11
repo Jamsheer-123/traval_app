e8f63ffddcfce5cbc4cc69ecee5e3c459dfe271e
mohammedmhd1333 <mohammedmhd1333@gmail.com>
2022-05-11 16:03:41 +0530

    message

--- traval_app/traval_app/doctype/passenger/passenger.py
+++ traval_app/traval_app/doctype/passenger/passenger.py
@@ -4,5 +4,11 @@
 # import frappe
 from frappe.model.document import Document
 
+
+def before_save(self):
+	self.arrival()
+
 class Passenger(Document):
 	pass
+def arrival(self):
+	arrival_date = frappe.get_single_value("Travel Category", "arrival_date")
