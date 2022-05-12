a188ef52765e15b678477c48fb37aa918486a25f
Jamsheer-123 <jamsheerak678@gmail.com>
2022-05-12 14:28:48 +0530

    fix: Travel Seat - Syntax Error

--- traval_app/traval_app/doctype/traval_seat/traval_seat.py
+++ traval_app/traval_app/doctype/traval_seat/traval_seat.py
@@ -3,8 +3,4 @@ from frappe.model.document import Document
 from frappe.model.docstatus import Docstatus
 
 class Travalseat(Document):
-'''	def beforesubmit():
-
-		total_seat = frappe.db.get_single_value("Travel Catogory", "total_seat")
-		no_passenger=frappe.db.get_single_value("Transaction", "no_passenger")
-		self.reserved_seat=
+	pass
